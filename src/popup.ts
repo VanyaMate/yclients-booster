e6f04ac2caf6aa32dc4e24e6822f72b6de843485
VanyaMate <buhu653@mail.ru>
2024-11-07 23:54:06 +0300

    update Component, update Popup

--- src/popup.ts
+++ src/popup.ts
@@ -1,44 +1,58 @@
-const app = document.querySelector('#app')!;
+import { Component } from '@/shared/component/Component.ts';
 
-app.innerHTML = `
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
 
-    body {
-        display: flex;
-        flex-direction: column;
-        align-items: safe center;
-        justify-content: safe center;
-        background: #fee;
-    }
-    
-    #app {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-        padding: 10px;
-        margin: 10px;
-        background: #fff;
-        border-radius: 10px;
-    }
-
-    h1 {
-        font-family: Consolas, 'monospace';
-        text-align: center;
-        color: #111;
-        font-size: 1.7em;
-    }
-    
-    video {
-        border-radius: 10px;
-    }
-</style>
-<h1>Балдежного вам котика</h1>
-<video autoplay loop> 
-    <source src="/sticker.webm" type="video/webm">
-</video>
-`;
+new Component<HTMLDivElement>('div', { id: 'app' }, [
+    new Component<HTMLStyleElement>('style', {
+        innerHTML: `
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+        
+            body {
+                display: flex;
+                flex-direction: column;
+                align-items: safe center;
+                justify-content: safe center;
+                background: #fee;
+            }
+            
+            #app {
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+                padding: 10px;
+                margin: 10px;
+                background: #fff;
+                border-radius: 20px;
+            }
+        
+            h1 {
+                font-family: Consolas, 'monospace';
+                text-align: center;
+                color: #111;
+                font-size: 1.7em;
+            }
+            
+            video {
+                border-radius: 10px;
+            }
+        `,
+    }),
+    new Component<HTMLHeadingElement>('h1', { innerHTML: 'Балдежного вам котика' }),
+    new Component<HTMLVideoElement>(
+        'video',
+        {
+            autoplay: true,
+            loop    : true,
+        },
+        [
+            new Component<HTMLSourceElement>('source', {
+                src : '/sticker.webm',
+                type: 'video/webm',
+            }),
+        ],
+    ),
+])
+    .insert(document.body, 'beforeend');
--- src/shared/component/Component.ts
+++ src/shared/component/Component.ts
@@ -11,8 +11,9 @@ export type ComponentPropsOptional<T extends HTMLElement> = Partial<ComponentPro
 export class Component<T extends HTMLElement> implements IComponent<T> {
     private readonly _element: T;
 
-    constructor (tag: string, props: ComponentPropsOptional<T>) {
+    constructor (tag: string, props: ComponentPropsOptional<T>, children: Array<Component<HTMLElement>> = []) {
         this._element = Object.assign(document.createElement(tag), props as ComponentProps<T>);
+        children.forEach((child) => child.insert(this.element, 'beforeend'));
     }
 
     get element () {
