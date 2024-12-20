d9d8a27d52ae6b3a517838e5bbad5f8d58691cd0
VanyaMate <buhu653@mail.ru>
2024-12-20 21:04:25 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,44 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
 import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 
-export default defineConfig({
-    plugins: [
-        /*        obfuscatorPlugin({
-         options: {
-         debugProtection: true,
-         // deadCodeInjection: true,         // NOT WORKING
-         optionsPreset: 'low-obfuscation',
-         renameGlobals: true,
-         // renameProperties : true,         // NOT WORKING
-         splitStrings         : true,
-         controlFlowFlattening: true,
-         transformObjectKeys  : true,
-         },
-         }),*/
-        crx({ manifest }),
-    ],
-    css    : {
-        modules: {
-            generateScopedName: '[name]_[local]_[hash:base64:5]',
+export default defineConfig(({ mode }) => {
+    const plugins: PluginOption[] = [];
+    const isProductionMode        = mode === 'production';
+    const cssScopeName            = isProductionMode ? '[hash:base64:5]'
+                                                     : '[name]_[local]_[hash:base64:5]';
+
+    if (isProductionMode) {
+        plugins.push(obfuscatorPlugin({
+            options: {
+                debugProtection: true,
+                // deadCodeInjection: true,         // NOT WORKING
+                optionsPreset: 'low-obfuscation',
+                renameGlobals: true,
+                // renameProperties : true,         // NOT WORKING
+                splitStrings         : true,
+                controlFlowFlattening: true,
+                transformObjectKeys  : true,
+            },
+        }));
+    }
+
+    plugins.push(crx({ manifest }));
+
+    return {
+        plugins: plugins,
+        css    : {
+            modules: {
+                generateScopedName: cssScopeName,
+            },
         },
-    },
-    resolve: {
-        alias: {
-            '@': '/src',
-            '$': '/',
+        resolve: {
+            alias: {
+                '@': '/src',
+                '$': '/',
+            },
         },
-    },
+    };
 });
