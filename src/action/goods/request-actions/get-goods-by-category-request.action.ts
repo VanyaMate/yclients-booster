88bdaf18e32a045e19e76fdc327d9678a04ee0e3
Vanya <buhu653@mail.ru>
2025-07-20 12:29:55 +0300

    🙏 Fix : Товары - Обновил функцию получения списка товаров категории. Раньше можно было получать сразу все одним запросом - теперь только по страницам

--- src/action/goods/request-actions/get-goods-by-category-request.action.ts
+++ src/action/goods/request-actions/get-goods-by-category-request.action.ts
@@ -5,25 +5,46 @@ import {
 
 
 export const getGoodsByCategoryRequestAction = async function (bearer: string, clientId: string, categoryId: string, logger?: ILogger): Promise<GoodApiResponseData> {
-    logger?.log(`получение списка товаров категории "${ categoryId }" клиента "${ clientId }"`);
+    logger?.log(`получение всех товаров категории "${ categoryId }" клиента "${ clientId }"`);
 
-    return fetch(`https://yclients.com/api/v1/goods/${ clientId }?category_id=${ categoryId }&count=999999`, {
-        method : 'GET',
-        headers: {
-            'Content-Type' : 'application/json',
-            'Authorization': `Bearer ${ bearer }`,
-        },
-    })
-        .then((response) => {
-            if (response.ok) {
-                logger?.success(`спискок товаров категории "${ categoryId }" клиента "${ clientId }" получен`);
-                return response.json();
-            }
+    const result: GoodApiResponseData = [];
+    const takeAmount                  = 25;
+    let page                          = 1;
+    let isFinish                      = false;
 
-            throw new Error(`ошибка ответа сервера. Статус: ${ response.status }`);
+    while (!isFinish) {
+        logger?.log(`получение списка товаров категории "${ categoryId }" клиента "${ clientId }" страницы "${ page }"`);
+        await fetch(`https://yclients.com/api/v1/goods/${ clientId }?category_id=${ categoryId }&count=${ takeAmount }&page=${ page }`, {
+            method : 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization': `Bearer ${ bearer }`,
+            },
         })
-        .catch((error: Error) => {
-            logger?.error(`спискок товаров категории "${ categoryId }" клиента "${ clientId }" не получен. ${ error.message }`);
-            throw error;
-        });
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+
+                throw new Error(`ошибка ответа сервера. Статус: ${ response.status }`);
+            })
+            .then((list: GoodApiResponseData) => {
+                result.push(...list);
+                logger?.success(`получено "${ list.length }" товаров из категории "${ categoryId }" клиента "${ clientId }" со страницы "${ page }"`);
+
+                if (list.length !== takeAmount) {
+                    logger?.success(`все товары категории "${ categoryId }" клиента "${ clientId }" получены`);
+                    isFinish = true;
+                } else {
+                    page += 1;
+                }
+            })
+            .catch((error: Error) => {
+                logger?.error(`список товаров категории "${ categoryId }" клиента "${ clientId }" страницы "${ page }" не получен. ${ error.message }`);
+                isFinish = true;
+                throw error;
+            });
+    }
+
+    return result;
 };
